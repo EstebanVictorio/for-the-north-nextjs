266c687cc472c96c3dce7163f5b11d3de6f3bbb4
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-21 09:39:24 -0500

    Updates

---
+++ pages-manifest-9f118aba30558f26427e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{140:function(a){a.exports={pages:[],dataPaths:{"about-f34":"708/path---about-f-34-4c2-0SUcWyAf8ecbYDsMhQkEfPzV8","basics-ec0":"339/path---basics-ec-0-26b-0SUcWyAf8ecbYDsMhQkEfPzV8","dev-404-page-5f9":"969/path---dev-404-page-5-f-9-fab-bfHDksSzZSy3u3R3XHmWzRikFY",index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8","posts-0d3":"593/path---posts-0-d-3-d29-0SUcWyAf8ecbYDsMhQkEfPzV8"}}}}]);
+//# sourceMappingURL=pages-manifest-9f118aba30558f26427e.js.map
