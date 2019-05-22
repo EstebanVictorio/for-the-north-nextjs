0fcfbfcfd9bda58cf74588d45b9a2d5afd234ac8
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-22 07:34:34 -0500

    [update] update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,6 @@ module.exports = {
   pathPrefix: "/for-the-north-nextjs",
   plugins: [
       {
-        resolve: `babel-plugin-styled-components`,
-        options: {
-          ssr: true,
-          displayName: false,
-        },
-      },
-      {
         resolve: `gatsby-plugin-styled-components`,
         options: {
           ssr: true,
