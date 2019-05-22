2c1230edd98b77e83ee9bfa6b43a8f364e14fd2c
Esteban Victorio <estebanvictorio92@gmail.com>
2019-05-21 23:20:39 -0500

    [update] update plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,10 +13,7 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-plugin-styled-components',
-      options: {
-        displayName: false,
-      }
+      resolve: 'gatsby-plugin-styled-components'
     }
   ]
 }
